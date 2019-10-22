c724c286e7d3e450570b569ca8b3a0095a7a55a0
Haden <hsantefort12@gmail.com>
2019-10-22 16:15:50 -0400

    Added hamburger menu

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,24 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../styles/styles.css';
+import HamMenu from './ham';
 
 
 export var Nav = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div className="container">
-                <Link to='/' className="navbar-brand">Schlegs Die</Link>
-                <div className="float-right">
-                    <div className="container">
-                        <Link to='/rules' className="navbar-item">Rules </Link>
-                    </div>
-                    <div className="container">
-                        <Link to='/gameStart' className="navbar-item">Play Game</Link>
-                    </div>
-                    <div className="container">
-                        <a href="https://github.com/hsantefort12/schlegs-die" className="navbar-item">Source Code</a>
-                    </div>
-                </div>
+            <div>
+                <HamMenu pageWrapId={"page-wrap"} outerContainerId={"App"} />
+            </div>
+            <div className="container title">
+                <Link to='/' className="navbar-brand">Schlegs Die</Link>   
             </div>
         </nav> 
     );
 }
+
+
+/*
+<div className="container">
+                    <Link to='/rules' className="navbar-item">Rules </Link>
+                </div>
+                <div className="container">
+                    <Link to='/gameStart' className="navbar-item">Play Game</Link>
+                </div>
+                <div className="container">
+                    <a href="https://github.com/hsantefort12/schlegs-die" className="navbar-item">Source Code</a>
+                </div>
+                */
